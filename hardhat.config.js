eb93a873822532169a790619fa65f4ef05208a88
Ubaid <ubaidshabbir.aptechiic@gmail.com>
2024-11-04 17:16:28 +0500

    add .env setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+const { INFURA_API_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
   networks: {
     hardhat: {
       forking: {
-        url: "https://mainnet.infura.io/v3/<key>",
+        url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+        blockNumber: 21109268
       }
     }
   }
